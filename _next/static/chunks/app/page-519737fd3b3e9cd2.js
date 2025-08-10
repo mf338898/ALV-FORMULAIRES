5af1b2c731451d6aa73789bfe7c8f189dfdd39f6
elfovo <152432815+elfovo@users.noreply.github.com>
2025-08-10 17:58:25 +0200

    Updates

---
+++ _next/static/chunks/app/page-519737fd3b3e9cd2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{4437:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,3063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[63,874,441,684,977],()=>s(4437)),_N_E=e.O()}]);
